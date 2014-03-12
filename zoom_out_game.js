feaea763f3deb8c815f17c1b6563365d0f0eb7d6
Thomas Fisher-York <tly2@cornell.edu>
2014-03-12 13:56:37 -0400

    When place is learned, replaces with a new place.

--- zoom_out_game.js
+++ zoom_out_game.js
@@ -10,19 +10,21 @@ function zoom_out_game(places) { //  this function is a constructor, so 'this' k
     var done_places = new Object;
     var current_places = new Object; // places.slice(0, n_places_at_a_time);
     var future_places = new Object; // places.slice(n_places_at_a_time, n_places_in_quiz);
-   // for (var i = 0; i < n_places_at_a_time; i++) { // current_places
-	for(var i in places){
+   for (var i = 0; i < n_places_at_a_time; i++) { // current_places
+//	for(var i in places){
         var the_place = places[i];
         current_places[the_place.name] = the_place;
         console.log("In current_places initialization. name, age: " + the_place.name + " ,  " + current_places[the_place.name].age);
     }
     for (var i = n_places_at_a_time; i < places.length; i++) { // future places
+	var age = 2*(places.length - i); //  
         var the_place = places[i];
+	the_place.age = age;
         future_places[the_place.name] = the_place;
     }
     initialize_places_ages(current_places); // initialize ages of current places to random order.
     // *************** CHOOSE INITIAL PLACE AT RANDOM *******************
-    var the_place_name = random_place(current_places, Math.random());
+    var the_place_name = random_place(current_places);
     var the_place = current_places[the_place_name];
 
     // ********************* CONSTRUCT MAP *****************************
@@ -104,24 +106,30 @@ function zoom_out_game(places) { //  this function is a constructor, so 'this' k
 
     // ************************  CREATE ANSWER BUTTONS  ***********************
     // they are text elements for now ....
-    var answer_buttons = new Array();
+    var answer_buttons = {}; // new Array();
     var answer_button_area = document.getElementById("answer_buttons");
 
     var current_names_array = Object.keys(current_places);
-    for (var i = 0; i < current_names_array.length; i++) {
-
-        var name_i = current_names_array[i];
-        answer_buttons[i] = document.createElement("a"); // using a link, not button at present.
+    console.log("current_names: " + current_names_array);
+   // for (var i = 0; i < current_names_array.length; i++) {
+    for(var i = 0; i<places.length; i++){
+	var a_place = places[i];
+        var name_i = a_place.name;
+        answer_buttons[name_i] = document.createElement("a"); // using a link, not button at present.
         //      answer_buttons[i] = document.createElement("BUTTON"); // using a link, not button at present.
-        var t = document.createTextNode(current_places[name_i].name);
-        answer_buttons[i].appendChild(t);
+        var t = document.createTextNode(name_i);
+        answer_buttons[name_i].appendChild(t);
         //    answer_buttons[i].textContent = current_places[i].name;
-        answer_buttons[i].href = "#"; // what does this do??? - makes it a link
-        answer_buttons[i].addEventListener("click", function(event) {
-            the_place = handle_answer_button_click(event, the_place, i);
+        answer_buttons[name_i].href = "#"; // what does this do??? - makes it a link
+        answer_buttons[name_i].addEventListener("click", function(event) {
+           the_place = 
+		handle_answer_button_click(event, the_place, answer_buttons);
         },
         false);
-        answer_button_area.appendChild(answer_buttons[i]);
+//	a_place.answer_button = answer_buttons[name_i];
+	if(name_i in current_places){
+        answer_button_area.appendChild(answer_buttons[name_i]);
+	}
     }
 
     // ******************* ADD EVENT LISTERNERS TO MAP *********************
@@ -144,9 +152,9 @@ function zoom_out_game(places) { //  this function is a constructor, so 'this' k
     });
 
     // ******************** HANDLE CLICKING ON ANSWER BUTTON ***************
-    function handle_answer_button_click(event, the_place, i) {
-        var txtnode = event.target.childNodes[0];
-        var txt = txtnode.nodeValue;
+    function handle_answer_button_click(event, the_place, answer_buttons){ //, i) {
+   //     var txtnode = event.target.childNodes[0];
+        var clicked_button_text =  event.target.childNodes[0].nodeValue;
 
         // *********  MAPLABEL WITH TEXT DISPLAYED TO INDICATE ANSWER IS RIGHT OR WRONG  **********
         var right_wrong_label = new MapLabel({
@@ -176,15 +184,53 @@ function zoom_out_game(places) { //  this function is a constructor, so 'this' k
             },
             icon: spot,
         });
+//	the_marker.position = the_place.marker_position.latlng;
+//	the_marker.labelContent = the_place.name;
 
         // **************** RESPOND TO CLICK ON ANSWER BUTTON ****************************     
         var delay = 800; // time allowed (ms) after correct answer before going to next question.
-        if (txt == the_place.name) { // ************ CORRECT ANSWER. this answer is correct. ************
+        if (clicked_button_text === the_place.name) { // ************ CORRECT ANSWER. this answer is correct. ************
             update_array(the_place.history, new Object({
                 correct: true,
                 zoom_clicks: zooms
-            })); // unshift and pop	
+            })); // unshift and pop the place's history	
             right_wrong_label.text = "Yes, it's ";
+	    console.log("SSSSSSSSSS place score: " + the_place.score);
+	console.log("BEFORE. future, current, done sizes: " + Object.keys(future_places).length + "; " +
+			    + Object.keys(current_places).length + "; " +
+			    + Object.keys(done_places).length);
+	    if(the_place.score >= place_sufficient_score){
+		// transfer the place from current to done places
+		if( (Object.keys(current_places).length > 0) && (Object.keys(future_places).length > 0) ){
+		delete current_places[the_place.name]; // delete the key/val pair from current places
+		done_places[the_place.name] = the_place; // add to done_places
+		console.log("future places: " + JSON.stringify(Object.keys(future_places)));
+		for(var nom in current_places){
+		    console.log("current name: "  + nom + "; " + current_places[nom].name);
+		}
+		// choose a place from future places and transfer it to current places
+		
+		
+	//	if(Object.keys(future_places).length > 0){ // !== undefined){
+var new_current_place_name = random_place(future_places);
+		var new_current_place = future_places[new_current_place_name];
+		new_current_place.age = Math.ceil(n_places_at_a_time/2);
+	//	console.log("Anew current place name: " + new_current_place.name);
+		delete future_places[new_current_place_name];		
+		current_places[new_current_place_name] = new_current_place;
+//	console.log("Bnew current place name: " + new_current_place.name);
+	console.log("AFTER. future, current, done sizes: " + Object.keys(future_places).length + "; " +
+			    + Object.keys(current_places).length + "; " +
+			    + Object.keys(done_places).length);
+	//	}
+
+		var answer_button_areax = document.getElementById("answer_buttons");
+		answer_button_areax.removeChild(answer_buttons[the_place.name]);
+        answer_button_areax.appendChild(answer_buttons[new_current_place_name]);
+		}
+
+	    }
+	    
         } else { // ************ WRONG ANSWER. this answer is wrong. ***********
             update_array(the_place.history, new Object({
                 correct: false,
@@ -194,13 +240,14 @@ function zoom_out_game(places) { //  this function is a constructor, so 'this' k
             delay = 2500;
         }
         the_marker.labelContent = the_place.name;
-        score += place_score(the_place, max_score_per_answer);
+        score += place_score_change(the_place, max_score_per_answer);
         document.getElementById("score_div").innerText = 'Score: ' + score;
         zooms = 0;
 
         // **************** GET NEXT PLACE **********************
         the_place_name = random_place(current_places); 
 	the_place = current_places[the_place_name];
+	console.log("the_place_name: " + the_place_name + " the place: " + the_place);
 
         the_place.age = 0;
         setTimeout(function() { // do some stuff after timeout of delay ms.
@@ -215,11 +262,11 @@ function zoom_out_game(places) { //  this function is a constructor, so 'this' k
 } // end of zoom_out_game (constructor)
 
 
-function place_score(place, max_score_per_answer) { // update the place's score, and return (new_score - old_score)
+function place_score_change(place, max_score_per_answer) { // update the place's score, and return (new_score - old_score)
     var old_place_score = place.score;
     var new_place_score = 0;
     for (var i = 0; i < place.history.length; i++) { // just
-        if (place.history[i].correct != true) {
+        if (place.history[i].correct !== true) {
             break;
         }
         new_place_score += Math.max(0, max_score_per_answer - place.history[i].zoom_clicks);
