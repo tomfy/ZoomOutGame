5a2a2f2f5a745b692120369c6bae7d72bf27724e
Thomas Fisher-York <tly2@cornell.edu>
2014-03-27 21:45:32 -0400

    Add a few arrays of countries.

--- Countries.js
+++ Countries.js
@@ -1,1 +1,72 @@
 var EuropeCountries = ['France', 'Germany', 'Italy', 'Spain', 'Belgium', 'Netherlands', 'Denmark', 'Poland', 'Czech Republic', 'Slovakia', 'Hungary', 'Romania', 'Serbia', 'Bosnia', 'Croatia', 'Slovenia', 'Portugal', 'Austria', 'Albania', 'Macedonia', 'Bulgaria', 'Greece', 'Turkey', 'Moldova', 'Ukraine', 'Russia', 'Lithuania', 'Latvia', 'Estonia', 'Finland', 'Sweden', 'Norway', 'United Kingdom', 'Ireland', 'Iceland', 'Luxembourg', 'Switzerland'];
+
+
+var BigAreaCountries = [ // top 50
+    Russia, Canada, China, United_States, Brazil,
+    Australia, India, Argentina, Kazakhstan, Algeria,
+
+    D_R_Congo, Greenland, Saudi_Arabia, Mexico, Indonesia, 
+    Sudan, // not including S. Sudan
+    Libya, Iran, Mongolia, Peru, 
+
+    Chad, Niger, Angola, Mali, South_Africa, 
+    Colombia, Ethiopia, Bolivia, Mauritania, Egypt,
+
+ //   United_Republic_of_Tanzania, // Official name, apparently
+    Tanzania, Nigeria, Venezuela, Pakistan, Namibia,
+    Mozambique, Turkey, Chile, Zambia, Myanmar_Burma,
+
+    Afghanistan, France, Somalia, Central_African_Republic, // South_Sudan,
+    Madagascar, Botswana, Kenya, Ukraine, Yemen,
+];
+
+var BigPopulationCountries = [ // top 50
+    China, India, United_States, Indonesia, Brazil,
+    Pakistan, Nigeria, Bangladesh, Russia, Japan,
+
+    Mexico, Philippines, Vietnam, Ethiopia, Egypt,
+    Germany, Iran, Turkey, D_R_Congo, Thailand,
+
+    France, United_Kingdom, Italy, Myanmar_Burma, South_Africa,
+    Republic_of_Korea, Colombia, Spain, Ukraine, Tanzania,
+
+    Kenya, Argentina, Algeria, Poland, Sudan,
+    Uganda, Canada, Iraq, Morocco, Peru,
+
+    Uzbekistan, Malaysia, Saudi_Arabia, Venezuela, Nepal,
+    Afghanistan, Yemen, Peoples_Republic_of_Korea, Ghana, Mozambique,
+];
+
+BigGdpCountries = [ // IMF 2012 PPP top 50
+    United_States, China, India, Japan, Germany,
+    Russia, Brazil, United_Kingdom, France, Italy,
+
+    Mexico, Republic_of_Korea, Canada, Spain, Indonesia,
+    Turkey, Iran, Australia, Saudi_Arabia, Taiwan,
+
+    Poland, Argentina, Netherlands, Thailand, South_Africa,
+    Egypt, Pakistan, Colombia, Malaysia, Nigeria,
+
+    Belgium, Philippines, Venezuela, Sweden, Switzerland, // Hong Kong between Sweden, Switzerland
+    Austria, Ukraine, Singapore, Peru, Vietnam, 
+
+    Chile, Bangladesh, Czech_Republic, Greece, Norway,
+    Algeria, Romania, United_Arab_Emirates, Israel, Portugal,
+];
+
+var most_european_countries = [
+    United_Kingdom, France, Ireland, Belgium, Luxembourg,
+    Netherlands, Spain, Switzerland, Italy, Germany,
+    Poland, Czech_Republic, Slovakia, Hungary, Slovenia,
+    Lithuania, Latvia, Estonia, Norway, Sweden,
+    Denmark, Finland, // Belarus, // Belarus absent from borders file for some reason. ??
+    Moldova, Ukraine, Romania,
+    Bulgaria, Croatia, Bosnia_and_Herzegovina, 
+ //   Serbia, Montenegro, // has Yugoslavia instead.
+    Yugoslavia,
+    Macedonia, Albania, Greece,
+];
+
+var few_european_countries = [ // a few - not too big, for testing purposes
+    Belgium, Luxembourg, Denmark, Switzerland, Czech_Republic
+]; 
