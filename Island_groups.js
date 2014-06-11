b5263c45a73907cb084676161d29dac1563baaf0
Thomas Fisher-York <tly2@cornell.edu>
2014-06-11 17:55:15 -0400

    Merge objs with borders with others for Philippines.

--- Island_groups.js
+++ Island_groups.js
@@ -118,6 +118,47 @@ var Indonesia_Islands_Populous = [ // ranked by population on whole 'island', no
     'Bintan', // Bintan: 330 thousand
 ];
 
+var Philippine_archipelago = [
+    'Luzon', 'Mindanao', 'Negros', 'Samar', 'Palawan', 
+    'Panay', 'Mindoro', 'Leyte', 'Cebu', 'Bohol',
+    'Masbate',
+
+    'Catanduanes', 'Basilan', 'Marinduque', 'Busuanga',
+    'Jolo', 'Dinagat', 'Polillo', 'TawiTawi', 'Guimaras', // 605 sq km
+    'Biliran', // 555 sq km
+// 21 so far - in order of size (area)
+  
+    'Sibuyan', // 445 sq km (172 sq mi)
+    'Siargao', // 437 sq km
+  'Burias', // 424 sq km
+ 'Tablas', // ? ~400 ?
+
+   'Culion', // 389 sq km
+  'Siquijor', // 337 sq km  
+    'Ticao', // 334 sq km
+   'Dumaran', // ? ~300 sq km ?
+    'Camiguin', // 238 sq km
+
+  // 30 so far
+  
+ // smaller than 200 sq km
+
+/*  'Philippines_Alabat', // 192 sq km 
+  'Philippines_Patnanongan', // 139 sq km
+  'Philippines_Bucas_Grande', // 128 sq km
+  'Philippines_Lubang', // 125 sq km */
+// 34 so far
+
+// smaller
+//  Philippines_Babuyan, // 2 outlines
+//  Philippines_Panaon,
+ // Philippines_Homonhon, // small
+//  Philippines_38etc, // 4
+ //   Philippines_20,
+ //   Philippines_23
+];
+
+
 var Indonesia_Islands_Big_or_Populous = [ // 25 islands.
 // > 100000 km^2
     'New_Guinea', 'Borneo', 'Sumatra', 'Sulawesi', 'Java',
