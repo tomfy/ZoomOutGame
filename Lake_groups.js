26a75eb2aa912a08eaf4aafda3ea5846dc8b6760
Thomas Fisher-York <tly2@cornell.edu>
2014-07-02 09:04:47 -0400

    Initial commit. Lake objects.

---
+++ Lake_groups.js
@@ -0,0 +1,4 @@
+var Big_Lakes_by_Area = [
+    'Caspian_Sea', 'Lake_Superior', 'Lake_Victoria', 'Lake_Huron', 'Lake_Michigan',
+    'Lake_Tanganyika', 'Lake_Baikal', 'Great_Bear_Lake', 'Lake_Malawi', 'Great_Slave_Lake',
+];
---
+++ Lakes.js
@@ -0,0 +1,419 @@
+// Lakes:
+
+Lakes =  {
+    Caspian_Sea: new Place({
+	name: "Caspian Sea",
+	area: {sqkm: 371000, rank: 1},   // are in km^2, and world rank by area
+	volume: {cukm: 78200, rank: 1},
+frame_center: {lat: 41.428, lng: 52.1703},
+marker_position: {lat: 41.8859, lng: 50.7129},
+zoom: 6,
+    }),
+
+    Lake_Superior: new Place({
+    name: "Lake Superior",
+	area: {sqkm: 82414, rank: 2},   // are in km^2, and world rank by area
+	volume: {cukm: 11600, rank: 4},
+frame_center: {lat: 47.5605, lng: -448.2336},
+marker_position: {lat: 47.695, lng: -87.984},
+zoom: 8,
+    }),
+
+    Lake_Victoria: new Place({
+	name: "Lake Victoria",
+		area: {sqkm: 69485, rank: 3},   // are in km^2, and world rank by area
+	volume: {cukm: 2700, rank: 9},
+frame_center: {lat: -1.2006, lng: 33.2024},
+marker_position: {lat: -1.0382, lng: 32.8931},
+zoom: 8,
+    }),
+
+    Lake_Huron: new Place({
+	name: "Lake Huron",
+	area:  {sqkm: 59600, rank: 4},
+	volume: {cukm: 3540, rank: 8},
+    frame_center: {lat: 44.6586, lng: -82.6594},
+marker_position: {lat: 44.9376, lng: -82.4139},
+zoom: 8,
+    }),
+
+    Lake_Michigan: new Place({
+	name: "Lake Michigan",
+	area: {sqkm: 58000, rank: 5},
+	volume: {cukm: 4920, rank: 7},
+frame_center: {lat: 44.2783, lng: -86.2025},
+marker_position: {lat: 44.1428, lng: -86.9678},
+zoom: 7,
+    }),
+
+    Lake_Tanganyika: new Place({
+	name: "Lake Tanganyika",
+	area:  {sqkm: 32893, rank: 6},
+	volume: {cukm: 18900, rank: 3},
+frame_center: {lat: -6.0344, lng: 29.6923},
+marker_position: {lat: -5.9767, lng: 29.5422},
+zoom: 7,
+    }),
+
+    Lake_Baikal: new Place({
+	name: "Lake Baikal",
+		area:  {sqkm: 31500, rank: 7},
+	volume: {cukm: 23600, rank: 2},
+frame_center: {lat: 53.5139, lng: 107.7227},
+marker_position: {lat: 53.3768, lng: 108.0615},
+zoom: 7,
+    }),
+
+    Great_Bear_Lake: new Place({
+	name: "Great Bear Lake",	
+	area:  {sqkm: 31080, rank: 8},
+	volume: {cukm: 2236, rank: 10},
+frame_center: {lat: 65.8712, lng: 239.1109},
+marker_position: {lat: 66.0493, lng: -120.9265},
+zoom: 7,
+    }),
+
+    Lake_Malawi: new Place({
+	name: "Lake Malawi",
+	area:  {sqkm: 30044, rank: 9},
+	volume: {cukm: 7725, rank: 5},
+frame_center: {lat: -11.9564, lng: 34.5592},
+marker_position: {lat: -12.1253, lng: 34.4092},
+zoom: 7,
+
+    }),
+
+    Great_Slave_Lake: new Place({
+	name: "Great_Slave_Lake",
+	area:  {sqkm: 28930, rank: 10},
+	volume: {cukm: 1580, rank: 13},
+frame_center: {lat: 61.8002, lng: 246.2191},
+marker_position: {lat: 61.7055, lng: -114.0161},
+zoom: 7,
+    }),
+
+    Lake_Erie: new Place({
+	name: "Lake Erie",
+	area:  {sqkm: 25719, rank: 11},
+	volume: {cukm: 545, rank: 18},
+frame_center: {lat: 42.0174, lng: -81.1557},
+marker_position: {lat: 42.139, lng: -81.3483},
+zoom: 8,
+    }),
+
+    Lake_Winnipeg: new Place({
+	name: "Lake Winnipeg",
+	area:  {sqkm: 23553, rank: 12},
+	volume: {cukm: 284, rank: 30},
+frame_center: {lat: 52.1408, lng: -97.6077},
+marker_position: {lat: 52.3957, lng: -97.6794},
+zoom: 7,
+
+    }),
+
+    Lake_Ontario: new Place({
+	name: "Lake Ontario",
+	area:  {sqkm: 19477, rank: 13},
+	volume: {cukm: 1710, rank: 12},
+frame_center: {lat: 43.6288, lng: -77.8817},
+marker_position: {lat: 43.6639, lng: -77.7832},
+zoom: 8,
+    }),
+
+    Lake_Balkhash: new Place({
+	name: "Lake Balkhash",
+	area:  {sqkm: 18428, rank: 14},
+	volume: {cukm: 112, rank: 33},
+frame_center: {lat: 45.2011, lng: 58.7896},
+marker_position: {lat: 45.2246, lng: 58.5516},
+zoom: 9,
+    }),
+
+    Lake_Ladoga: new Place({
+	name: "Lake Ladoga",
+	area:  {sqkm: 18130, rank: 15},
+	volume: {cukm: 908, rank: 14},
+frame_center: {lat: 60.7891, lng: 31.3224},
+marker_position: {lat: 60.8074, lng: 31.4758},
+zoom: 8,
+    }),
+
+    Lake_Vostok: new Place({
+	name: "Lake Vostok",
+	area:  {sqkm: 15690, rank: 16},
+	volume: {cukm: 5400, rank: 6},
+
+    }),
+
+    Lake_Onega: new Place({
+	name: "Lake Onega",
+	area:  {sqkm: 9891, rank: 17},
+	volume: {cukm: 295, rank: 21},
+frame_center: {lat: 61.8618, lng: 35.1786},
+marker_position: {lat: 61.7601, lng: 35.3156},
+zoom: 8,
+    }),
+
+    Lake_Titicaca: new Place({
+	name: "Lake Titicaca",
+	area:  {sqkm: 8135, rank: 18},
+	volume: {cukm: 893, rank: 15},
+frame_center: {lat: -15.86, lng: -69.3399},
+marker_position: {lat: -15.853, lng: -69.3484},
+zoom: 9,
+    }),
+
+    Lake_Nicaragua: new Place({
+	name: "Lake Nicaragua",
+	area:  {sqkm: 8001, rank: 19},
+	volume: {cukm: 108, rank: 35},
+frame_center: {lat: 11.4702, lng: -85.3854},
+marker_position: {lat: 11.5123, lng: -85.416},
+zoom: 9,
+    }),
+
+    Lake_Athabasca: new Place({
+	name: "Lake Athabasca",
+	area:  {sqkm: 7920, rank: 20},
+	volume: {cukm: 110, rank: 34},
+frame_center: {lat: 59.1631, lng: 250.8059},
+marker_position: {lat: 59.2799, lng: -109.4678},
+zoom: 8,
+    }),
+
+    Lake_Taymyr: new Place({
+	name: "Lake Taymyr",
+	area:  {sqkm: 6990, rank: 21},
+frame_center: {lat: 74.4754, lng: 102.0881},
+marker_position: {lat: 74.5316, lng: 101.6565},
+zoom: 7,
+    }),
+
+    Lake_Turkana: new Place({
+	name: "Lake Turkana",
+	area:  {sqkm: 6405, rank: 22},
+	volume: {cukm: 204, rank: 24},
+frame_center: {lat: 3.598, lng: 36.0534},
+marker_position: {lat: 3.656, lng: 36.0461},
+zoom: 8,
+    }),
+
+    Reindeer_Lake: new Place({
+	name: "Reindeer Lake",
+	area:  {sqkm: 6330, rank: 23},
+frame_center: {lat: 57.1233, lng: -102.2796},
+marker_position: {lat: 57.3354, lng: -102.3431},
+zoom: 8,
+    }),
+
+    Issyk_Kul: new Place({
+	name: "Issyk-Kul",
+	area:  {sqkm: 6200, rank: 24},
+	volume: {cukm: 1730, rank: 11},
+frame_center: {lat: 45.2011, lng: 58.7896},
+marker_position: {lat: 45.2246, lng: 58.5516},
+zoom: 9,
+    }),
+
+    Lake_Urmia: new Place({
+	name: "Lake Urmia",
+	area:  {sqkm: 6001, rank: 25},
+frame_center: {lat: 37.6911, lng: 45.3766},
+marker_position: {lat: 37.7273, lng: 45.3598},
+zoom: 9,
+    }),
+
+    Lake_Vanern: new Place({
+	name: "Lake Vanern",
+	area:  {sqkm: 5545, rank: 26},
+	volume: {cukm: 153, rank: 26},
+frame_center: {lat: 58.9066, lng: 13.3982},
+marker_position: {lat: 58.8592, lng: 13.3319},
+zoom: 8,
+    }),
+
+    Lake_Winnepegosis: new Place({
+	name: "Lake Winnipegosis",
+	area:  {sqkm: 5403, rank: 27},
+frame_center: {lat: 52.3997, lng: -99.794},
+marker_position: {lat: 50.5937, lng: -98.9923},
+zoom: 8,
+    }),
+
+    Lake_Albert: new Place({
+	name: "Lake Albert",
+	area:  {sqkm: 5299, rank: 28},
+	volume: {cukm: 132, rank: 29},
+frame_center: {lat: 1.694, lng: 30.8843},
+marker_position: {lat: 1.6999, lng: 30.943},
+zoom: 9,
+    }),
+
+    Lake_Mweru: new Place({
+	name: "Lake Mweru",
+	area:  {sqkm: 5120, rank: 29},
+frame_center: {lat: -9.0537, lng: 28.7832},
+marker_position: {lat: -8.98, lng: 28.732},
+zoom: 9,
+
+    }),
+
+    Lake_Nettilling: new Place({
+	name: "Lake Nettilling",
+	area:  {sqkm: 5066, rank: 30},
+	volume: {cukm: 114, rank: 32},
+frame_center: {lat: 66.5355, lng: -70.5374},
+marker_position: {lat: 66.522, lng: -70.5927},
+zoom: 8,
+    }),
+
+  Lake_Nipigon: new Place({
+	name: "Lake Nipigon",
+	area:  {sqkm: 4843, rank: 31},
+	volume: {cukm: 165, rank: 25},
+frame_center: {lat: 49.7961, lng: -88.478},
+marker_position: {lat: 49.7742, lng: -88.558},
+zoom: 9,
+    }),
+
+    Lake_Manitoba: new Place({
+	name: "Lake Manitoba",
+	area:  {sqkm: 4706, rank: 32},
+frame_center: {lat: 50.9036, lng: -98.7063},
+marker_position: {lat: 50.7504, lng: -98.5583},
+zoom: 8,
+    }),
+
+    Great_Salt_Lake: new Place({
+	name: "Great Salt Lake",
+	area:  {sqkm: 4662, rank: 33},
+frame_center: {lat: 41.1711, lng: -112.4969},
+marker_position: {lat: 41.1228, lng: -112.5082},
+zoom: 9,
+    }),
+
+    Qinghai_Lake: new Place({
+	name: "Qinghai Lake",
+	area:  {sqkm: 4489, rank: 34},
+frame_center: {lat: 36.8815, lng: 100.1888},
+marker_position: {lat: 36.8928, lng: 100.1816},
+zoom: 10,
+    }),
+
+    Lake_Saimaa:  new Place({
+	name: "Lake Saimaa",
+	area:  {sqkm: 4400, rank: 35},
+frame_center: {lat: 61.3406, lng: 28.2407},
+marker_position: {lat: 61.2808, lng: 28.2458},
+zoom: 9,
+    }),
+
+    Lake_of_the_Woods:  new Place({
+	name: "Lake of the Woods",
+	area:  {sqkm: 4350, rank: 36},
+frame_center: {lat: 49.1662, lng: -94.6798},
+marker_position: {lat: 49.135, lng: -94.8395},
+zoom: 8,
+
+
+    }),
+
+    Lake_Khanka:  new Place({
+	name: "Lake Khanka",
+	area:  {sqkm: 4190, rank: 37},
+frame_center: {lat: 44.913, lng: 132.4845},
+marker_position: {lat: 44.9901, lng: 132.4017},
+zoom: 9,
+    }),
+// Lakes large by volume not found above:
+
+    Lake_Van: new Place({
+	name: "Lake Van",
+	area:  {sqkm: 3755, rank: 10000},
+	volume: {cukm: 607, rank: 16},
+frame_center: {lat: 38.6198, lng: 42.9679},
+marker_position: {lat: 38.619, lng: 42.9016},
+zoom: 9,
+    }),
+    
+    Lake_Kivu: new Place({
+	name: "Lake Kivu",
+	area:  {sqkm: 2700, rank: 10000},
+	volume: {cukm: 569, rank: 17},
+frame_center: {lat: -2.0613, lng: 29.1141},
+marker_position: {lat: -1.984, lng: 29.1316},
+zoom: 10,
+    }),
+
+    Lake_Khovsgol: new Place({
+	name: "Lake Khovsgol",
+	area:  {sqkm: 2760, rank: 10000},
+	volume: {cukm: 480, rank: 19},
+rame_center: {lat: 53.5139, lng: 107.7227},
+marker_position: {lat: 53.3768, lng: 108.0615},
+zoom: 7,
+    }),
+
+    Lake_Toba: new Place({
+	name: "Lake Toba",
+	area:  {sqkm: 1130, rank: 10000},
+	volume: {cukm: 240, rank: 22},
+frame_center: {lat: 2.5757, lng: 98.786},
+marker_position: {lat: 2.7771, lng: 98.6462},
+zoom: 10,
+    }),
+
+    Lake_Argentino: new Place({
+	name: "Lake Argentino",
+	area:  {sqkm: 1466, rank: 10000},
+	volume: {cukm: 220, rank: 16},
+frame_center: {lat: -50.3102, lng: 287.3979},
+marker_position: {lat: -49.5715, lng: -70.7108},
+zoom: 9,
+    }),
+
+    Lake_Tahoe: new Place({
+	name: "Lake Tahoe",
+	area:  {sqkm: 496, rank: 10000},
+	volume: {cukm: 151, rank: 27},
+frame_center: {lat: 39.093, lng: 240.0303},
+marker_position: {lat: 39.0992, lng: -120.0325},
+zoom: 10,
+    }),
+
+    Dead_Sea: new Place({
+	name: "Dead Sea",
+	area:  {sqkm: 810, rank: 10000},
+	volume: {cukm: 147, rank: 28},
+frame_center: {lat: 31.3662, lng: 35.4848},
+marker_position: {lat: 31.4544, lng: 35.4762},
+zoom: 10,
+    }),
+
+    Lake_Iliamna: new Place({
+	name: "Lake Iliamna",
+	area:  {sqkm: 2622, rank: 10000},
+	volume: {cukm: 115, rank: 31},
+frame_center: {lat: 59.5449, lng: -154.9282},
+marker_position: {lat: 59.5357, lng: -155.1462},
+zoom: 9,
+    }),
+    
+    North_Aral_Sea: new Place({
+	name: "North Aral Sea",
+	area: {sqkm: 3300, rank: 10000}, // not in wikipedia's list by area 
+frame_center: {lat: 46.4101, lng: 60.6723},
+marker_position: {lat: 46.4369, lng: 60.6761},
+zoom: 10,
+
+    }),
+
+     West_Aral_Sea: new Place({
+	name: "West Aral Sea",
+	 area: {sqkm: 3500, rank: 10000}, // not in wikipedia's list by area 
+frame_center: {lat: 45.2011, lng: 58.7896},
+marker_position: {lat: 45.2246, lng: 58.5516},
+zoom: 9,
+     }),
+
+}; // end of Lakes
