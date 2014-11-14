e14178a6d1ecd8c748eefe3b85c8512a85f9b8e3
Thomas Fisher-York <tly2@cornell.edu>
2014-11-14 16:00:29 -0500

    Added lake groups for great lakes (africa, NA) and big (area) lakes.

--- Lake_groups.js
+++ Lake_groups.js
@@ -1,4 +1,23 @@
 var Big_Lakes_by_Area = [
-    'Caspian_Sea', 'Lake_Superior', 'Lake_Victoria', 'Lake_Huron', 'Lake_Michigan',
+   /* 'Caspian_Sea', 'Lake_Superior', 'Lake_Victoria', 'Lake_Huron', 'Lake_Michigan',
     'Lake_Tanganyika', 'Lake_Baikal', 'Great_Bear_Lake', 'Lake_Malawi', 'Great_Slave_Lake',
+    'Lake_Erie', 'Lake_Winnipeg', 'Lake_Ontario', 'Lake_Balkhash', 'Lake_Ladoga', */
+    'Lake_Vostok', 'Lake_Onega', 'Lake_Titicaca', 'Lake_Nicaragua', 'Lake_Athabasca', 
+    'Lake_Taymyr', 'Lake_Turkana', 'Reindeer_Lake', 'Issyk_Kul', 'Lake_Urmia',
+    'Lake_Vanern', 'Lake_Winnipegosis', 'Lake_Albert', 'Lake_Mweru', 'Lake_Nettilling',
+    'Lake_Nipigon', 'Lake_Manitoba', 'Great_Salt_Lake', 'Qinghai_Lake', 'Saimaa', 
+    'North_Aral_Sea', 'West_Aral_Sea',  // Where do they belong in the list?
+    'Lake_of_the_Woods', 'Lake_Khanka',
+    'Lake_Bangweulu', 'Lake_Chad', // shallow, seasonally variable
+    'Lake_Kivu', 'Lake_Van', 'Lake_Khovsgol', 'Lake_Argentino', 'Lake_Tahoe',
+    'Lake_Toba', 'Dead_Sea', 'Lake_Iliamna',
+];
+
+var Great_Lakes_NA = [
+    'Lake_Superior', 'Lake_Huron', 'Lake_Michigan', 'Lake_Erie', 'Lake_Ontario'
+];
+
+var Great_Lakes_Africa = [
+    'Lake_Victoria', 'Lake_Tanganyika', 'Lake_Malawi', 'Lake_Turkana', 'Lake_Albert',
+    'Lake_Mweru', 'Lake_Kivu',
 ];
